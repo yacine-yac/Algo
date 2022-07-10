29afa36983f95cbb39e21aced89d2d0d942e9890
yacine <lentente1958@gmail.com>
2022-07-10 23:43:14 +0100

    define methods

--- linkedlist.js
+++ linkedlist.js
@@ -6,10 +6,16 @@ function node(value,next=null){
 }
 function list(head=null){
     this.head=head; 
+    this.size=function(){};
+    this.Lastnode=function(){};
+    this.firstnode=function(){};
+    this.clear=function(){}
 }
 
 let j1=new node(1);
 let j2=new node(2);
 j1.next=j2;
+
+let lista=new list(j1);
 console.log(j2);
 
