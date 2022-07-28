815e3ff49975a262e3aefeaebcaea2f60528aa69
yacine <lentente1958@gmail.com>
2022-07-28 23:46:31 +0100

    add Stack class and add method

--- stack/stack.js
+++ stack/stack.js
@@ -1,1 +1,15 @@
-console.log('topppp');
+class Stack{
+    constructor(){
+       
+        this.data=[]; this.length=0;
+    }
+    add(value){
+           this.data.push(value);
+           this.data++;
+           return [value];
+    }
+}
+let stack=new Stack();
+console.log(stack);
+console.log(stack.add(55));
+console.log(stack.data,stack.length);
