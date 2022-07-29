9ef6925daae59d4269545d39be41aed925ab3116
yacine <lentente1958@gmail.com>
2022-07-29 12:36:30 +0100

    define add Dequeue and isEmprty methods

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,6 +1,27 @@
 class Queue{
     constructor(){
-        this.REAR=-1,this.FRONT=-1;
+        this.REAR=-1,this.FRONT=-1,this.length=0;
         this.data=[];
     }
+    add(value){
+       this.REAR++,this.length++;
+       this.FRONT=== -1 && this.FRONT++ ;
+       this.data[this.REAR]=value;
+       return this.data;
+    }
+    Dequeue(){
+          this.length--,this.REAR--;
+          this.REAR ===-1 && this.FRONT--;
+       return  this.data.splice(this.FRONT,1);
+    }
+    isEmpty(){
+         return this.REAR !==-1 ? false : true;
+    }
 }
+let r=new Queue();
+r.add(7);
+r.add(15);
+r.add(40);
+
+console.log(r);
+console.log(r.Dequeue(),r);
