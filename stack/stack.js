d46f1ce9339025b3395691ac2fd4548333c3835f
yacine <lentente1958@gmail.com>
2022-07-29 11:10:20 +0100

    add TOP pointer

--- stack/stack.js
+++ stack/stack.js
@@ -1,26 +1,27 @@
 class Stack{
     constructor(){
        
-        this.data=[]; this.length=0;
-      
+        this.data=[]; this.length=0; this.TOP=-1;
+      //TOP is a pointerwich track the top element in the stack 
     }
     add(value){
            this.data.push(value);
-           this.length++;
+           this.length++,this.TOP++;
            return [value];
     }
     isEmpty(){  
         // check if data is empty or no 
         // empty ==> return true 
         // not empty ==> false 
-        return  this.length>0 ? false :true;
+        return  this.TOP !=-1 ? false :true;
     }
     pop(){ // return the deleted item (the last item)
+        this.length--,this.TOP--;
         return   this.data.pop();  
     }
     peek(){
         // this method return the most element in the stack 
-           return this.data[this.length-1];
+           return this.data[this.TOP];
     }
     get(position){
         // this method return a specific item by his position in array
@@ -28,6 +29,7 @@ class Stack{
           return this.data[position] ?? undefined;
     }
 }
+// some operations on a stack DS
 let stack=new Stack();
 console.log(stack);
 console.log(stack.add(55));
